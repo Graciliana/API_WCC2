978910142b19061785e8ffbeff623d63924cdb14
Graciliana <eagraciliana@gmail.com>
2021-06-02 17:37:04 -0300

    criando metodo post/inserir

--- controllers/routes.js
+++ controllers/routes.js
@@ -1,6 +1,15 @@
+const Agendamento = require('../models/Agendamento')
 
-//O express nos fornece 2 coisas: req e resp
-//Req o que estamos recebendo e Resp o que iremos enviar
-module.export = app.get('/', (req, resp) =>{
-    res.send('Servidor OK')
-});
+module.export = app => {
+    //O express nos fornece 2 coisas: req e resp
+    //Req o que estamos recebendo e Resp o que iremos enviar
+    app.get('/', (req, resp) =>{
+        res.send('Servidor OK')
+    });
+
+    app.post('/agendar', (req,resp) => {
+        //verificar o que está sendo recebido
+        const agendamento = req.body;
+        console.log(agendamento)
+    })
+};
