4e9d7165491ea6cf8eb59f56bc53d1e5f80d0ac5
Graciliana <eagraciliana@gmail.com>
2021-06-02 16:59:08 -0300

    criando tabela Agendaemnto

--- config/configExpress.js
+++ config/configExpress.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const consign = require('consign');
+const bodyParser = require('body-parser');
 
 module.exports = () => {
     //Criando nossa aplicação
     const app = express();
+
+    //use() é utilizado para carregar libs dentro do express, para Ler o body html da requisição
+    app.use(bodyParser.urlencoded({
+        extended: true
+    }));
 
     //use() é utilizado para carregar libs dentro do express, para Ler o body em json da requisição
     app.use(bodyParser.json());
